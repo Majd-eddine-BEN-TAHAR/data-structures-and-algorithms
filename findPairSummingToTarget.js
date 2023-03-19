ed3438d5cd0d43a2d5264ef6fbdda5c3ac30d9c1
= <majdeddinebentahar@gmail.com>
2023-03-19 11:10:51 +0100

    #5- finish findPairSummingToTarget

---
+++ findPairSummingToTarget.js
@@ -0,0 +1,15 @@
+function findPairSummingToTarget(arr, target) {
+  const freqCounter = {};
+  for (let val of arr) {
+    if (freqCounter[target - val]) {
+      return [val, target - val];
+    }
+    freqCounter[val] = true;
+  }
+  return null;
+}
+
+console.log(findPairSummingToTarget([1, 2, 3, 4, 5], 7)); // [2, 5]
+console.log(findPairSummingToTarget([10, 20, 30, 40, 50], 60)); // [10, 50]
+console.log(findPairSummingToTarget([5, 10, 15, 20], 30)); // [10, 20]
+console.log(findPairSummingToTarget([1, 2, 3], 7)); // null
