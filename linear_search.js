8ed863613997bf47888150bf8ea6a59196a8fd5d
Majd Eddine BEN TAHAR <majdeddinebentahar@gmail.com>
2023-03-25 11:24:34 +0100

    transfer the file to the search-algorithms folder

--- linear_search.js
+++
@@ -1,9 +0,0 @@
-function linearSearch(arr, n) {
-  for (let index = 0; index < arr.length; index++)
-    if (arr[index] === n) return index;
-
-  return -1;
-}
-
-console.log(linearSearch([1, 6, 4, 2], 4)); // 2
-console.log(linearSearch([2, 9, 5, 0], 2)); // 0
