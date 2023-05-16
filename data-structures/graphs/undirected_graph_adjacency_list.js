7824283a8e7f119c1eff35bc60ceb5db1a133295
Majd Eddine BEN TAHAR <majdeddinebentahar@gmail.com>
2023-05-16 19:25:54 +0100

    implemented 2 graph traversal methods (#55)

--- data-structures/graphs/undirected_graph_adjacency_list.js
+++ data-structures/graphs/undirected_graph_adjacency_list.js
@@ -47,18 +47,105 @@ class UndirectedGraphList {
     // Deletes the vertex from the AdjacencyList object
     delete this.AdjacencyList[vertex];
   }
+  printGraph() {
+    for (const vertex in this.AdjacencyList) {
+      const edges = this.AdjacencyList[vertex].join(", ");
+      console.log(`${vertex} -> ${edges}`);
+    }
+  }
+
+  // it's to you to choose which vertix to start from traversing the graph
+  recursiveDepthFirstTraversal(startingVertex) {
+    if (!this.AdjacencyList[startingVertex]) {
+      return "starting vertex doesn't exist";
+    }
+
+    // i created adjacencyList variable to avoid losing the context of this keyword when it's used in a closure, you can remove it if you want but make sure to use an arrow function for the traverse function instead
+    const adjacencyList = this.AdjacencyList;
+    const visited = {};
+    const result = [];
+
+    function traverse(vertex) {
+      visited[vertex] = true; // Mark the vertex as visited
+      result.push(vertex);
+
+      // Traverse each connected vertex of the current vertex
+      for (const connectedVertex of adjacencyList[vertex]) {
+        if (!visited[connectedVertex]) traverse(connectedVertex);
+      }
+    }
+
+    traverse(startingVertex);
+
+    return result;
+  }
+
+  iterativeDepthFirstTraversal(startingVertex) {
+    const stack = [startingVertex];
+    const result = [];
+    const visited = {};
+
+    // mark the starting veertex as visited
+    visited[startingVertex] = true;
+
+    while (stack.length > 0) {
+      let currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.AdjacencyList[currentVertex].forEach((neighbor) => {
+        //  If you added a vertex to the stack to iterate over its neighbors, there's no need to push it into the stack again.
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+
+  breadthFirstTraversal(startingVertex) {
+    const visited = {}; // Keep track of visited vertices
+    const queue = [startingVertex]; // Initialize the queue with the starting vertex
+    const result = [];
+
+    while (queue.length > 0) {
+      const currentVertex = queue.shift(); // Dequeue the front vertex from the queue
+      result.push(currentVertex);
+
+      for (const neighbor of this.AdjacencyList[currentVertex]) {
+        if (!visited[neighbor]) {
+          queue.push(neighbor);
+          visited[currentVertex] = true; // Mark the current vertex as visited
+        }
+      }
+    }
+
+    return result;
+  }
 }
 
 const graph = new UndirectedGraphList();
 
 // Add vertices to the graph
-graph.addVertex(3);
-graph.addVertex(2);
-graph.addVertex(4);
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
 
-// Add edges between vertices
-graph.addEdge(4, 3);
-graph.addEdge(2, 4);
+// // Add edges between vertices
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("B", "E");
+graph.addEdge("C", "F");
 
 // Remove vertex 2 and its associated edges
-graph.removeVertex(2);
+// graph.removeVertex(2);
+
+// print the graph
+graph.printGraph();
+console.log(graph.recursiveDepthFirstTraversal("A")); // [ 'A', 'B', 'D', 'E', 'C', 'F' ]
+console.log(graph.iterativeDepthFirstTraversal("A")); // [ 'A', 'C', 'F', 'B', 'E', 'D' ]
+console.log(graph.breadhFirstTraversal("A")); //         [ 'A', 'B', 'C', 'D', 'E', 'F' ]
